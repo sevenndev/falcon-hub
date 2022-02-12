9a2a4ddf9c4780f1990c9e8eef8e5c847031ef6f
syntaxlol <92194055+tripru@users.noreply.github.com>
2022-02-12 12:37:25 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 286090429 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/tripru/falcon-hub/main/arsenal.lua"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/tripru/falcon-hub/main/falcon hub.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/tripru/falcon-hub/main/main.lua"))()
 end
